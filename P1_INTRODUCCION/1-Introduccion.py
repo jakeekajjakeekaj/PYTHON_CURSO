7d17aa7658c5b085f93be51d502835250213ebbb
jakeekajjakeekaj <yaricklozanomoctezuma1@gmail.com>
2024-02-23 18:00:23 -0600

    first commit

---
+++ P1_INTRODUCCION/1-Introduccion.py
@@ -0,0 +1,27 @@
+# print ("hola mundo");
+# print(2+2);
+
+"string"
+'string'
+
+"""string:
+    string
+"""
+'''string:
+    string
+'''
+
+# COMO SE PUEDE VER ARRIBA, ESAS SON LAS MANERAS EN QUE SE PUEDE REPRESENTAR A LOS STRING, Y CON LAS 3 COMILLAS PODEMOS TENER SALTOS DE LINEA EN NUESTROS STRING Y NO PASA NADA.
+
+# EL EQUIVALENTE A USAR `${variable}` DE JS A PYTHON ES: f"Hola {variable} cómo estás?"
+
+# Si queremos eliminar a una variable utilizamos: del variable
+
+# Pertenencia, es decir:
+# nombre = "Mario"
+# bienvenida = f"Hola {nombre} ¿cómo estás?"
+# print("Mario" in bienvenida)    #True
+# print("Mario" not in bienvenida)    #False
+
+# Esto es porque el primer verifica que Mario se encuentra en bienvenida? la respuesta es si, y en el segundo caso verifica que Mario no se encuentra en bienvenida, y la respuesta es false ya que Mario si se encuentra en bienvenida
+
